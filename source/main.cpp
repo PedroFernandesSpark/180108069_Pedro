8f59a83f1b21dbe5c536b9a39ffe9b8940de1982
PedroFernandesSpark <pedrofernandesspark@gmail.com>
2020-10-16 20:39:54 -0300

    adicionado cpplint e adotado o padrao de programacao do google passadno em todos os testes do cpplint, falta makefile

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,9 @@
-#define CATCH_CONFIG_MAIN 
+// copyright [2020] <Pedro Fernandes>
+#ifndef SOURCE_STACK_HPP_
+#define CATCH_CONFIG_MAIN
+#define SOURCE_STACK_HPP_
 #include "catch.hpp"
-#include "tests.cpp"
+#include "tests.hpp"
+#endif
 
 
--- source/stack.hpp
+++ source/stack.hpp
@@ -1,23 +1,29 @@
-#include <iostream>
-#include <cstdlib>
-using namespace std;
+// copyright [2020] <Pedro Fernandes>
+#ifndef SOURCE_STACK_HPP_
+#define SOURCE_STACK_HPP_
+#include "catch.hpp"
+#endif  // SOURCE_STACK_HPP_
 
+// Classe que contem cada elemtendo da pilha
+// o tipo da variavel StackItem pode ser mudada para mudar o tipo
+// do item da pilha
 class stackItemClass {
-public:
+ public:
     int StackItem;
 };
 
+
+// Classe que define o funcionamento da pilha
 class stackArray {
-private:
+ private:
     stackItemClass * pilha;
     int stackItem;
     int topo;
     int capacity;
     stackItemClass ErrorCase;
 
-public:
-    stackArray(int size = 10);
-
+ public:
+    void CreateStack(int size);
     void Push(int item);
     stackItemClass Pop();
     stackItemClass Top();
@@ -28,59 +34,62 @@ public:
     bool DestroyStack(stackArray stack);
 };
 
-stackArray::stackArray(int size) {
-    pilha = (stackItemClass*) malloc(size*sizeof(stackItemClass));
+// Metodo que cria e define o tamanho da pilha
+void stackArray::CreateStack(int size) {
+    pilha = reinterpret_cast <stackItemClass*>
+    (malloc(size*sizeof(stackItemClass)));
     topo = -1;
     capacity = size;
 }
 
+// Metodo que insere um novo item na pilha
 void stackArray::Push(int item) {
-    if (!IsFull()){
+    if (!IsFull()) {
     topo++;
     pilha[topo].StackItem = item;
-    cout << "Inserting:   " << item << endl;
-    }
-    else cout << "Stack is full";
+    } else {
+        return; }
 }
 
+// Metodo que retira o item que esta no topo da pilha
 stackItemClass stackArray::Pop() {
     ErrorCase.StackItem = -2;
     stackItemClass intermed;
-    if (!IsEmpty()){
-        cout << "popping" << Top().StackItem << endl;
+    if (!IsEmpty()) {
         intermed = pilha[topo];
         topo = topo -1;
         return intermed;
-    }
-    else
-    {
-        cout << "Stack is Empty";
+    } else {
         return ErrorCase;
     }
-    
 }
 
+
+// Metodo que verifica e retorna qual o item que esta no topo da pilha
 stackItemClass stackArray::Top() {
     ErrorCase.StackItem = -2;
     if (!IsEmpty()) {
         return pilha[topo];
-    }
-    else {
-        cout << "Stack is empty";
+    } else {
         return ErrorCase;
     }
 }
 
+// Metodo que muda o tamanho da pilha
 bool stackArray::SetSize(int newSize) {
-    pilha = (stackItemClass*) realloc(pilha, newSize*sizeof(stackItemClass));
+    pilha = reinterpret_cast <stackItemClass*>
+     (realloc(pilha, newSize*sizeof(stackItemClass)));
     capacity = newSize;
     return true;
 }
 
+
+// Metodo que retorna o tamanho atual da pilha
 int stackArray::Size() {
     return capacity;
 }
 
+// Metodo que verifica se a pilha esta vazia
 bool stackArray::IsEmpty() {
     if (topo <= -1)
         return 1;
@@ -88,13 +97,16 @@ bool stackArray::IsEmpty() {
         return 0;
 }
 
+// Metodo que verifica se a pilha esta cheia
 bool stackArray::IsFull() {
     if (capacity == topo - 1)
         return 1;
     else
         return 0;
 }
-bool stackArray::DestroyStack(stackArray stack){
+
+// Metodo que destroi a pilha
+bool stackArray::DestroyStack(stackArray stack) {
     delete stack.pilha;
     stack.topo = -1;
     return true;
